fe33321790fa6003e732a63307a62ff06db1c353
RomPen <73342065+RomPen@users.noreply.github.com>
2022-09-26 11:25:22 +0100

    Update streamlit_app.py
    byos

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header("Breakfast Menu:")
 streamlit.text("Omega3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-Range Eggs")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
