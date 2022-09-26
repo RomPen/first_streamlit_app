1073858d85c402731fe75c1033e9b5e132d1b0f0
RomPen <73342065+RomPen@users.noreply.github.com>
2022-09-26 11:26:41 +0100

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-Range Eggs")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
